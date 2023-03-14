891058da5a9dd48159b22be011bd23a14695da21
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-03-14 04:52:07 +0100

    Fixed, and deployed

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <ToggleSwitch />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+   
+  }
+};
+</script>
+
+<style>
+  .app{
+    width: 100vw;
+    max-width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,50 @@
+<template>
+    <main class='error'>
+      <div class='image_wrapper'>  
+        <img src='@/assets/error.jpg' />
+      </div>
+      <!-- <h1 class='heading-main'>404 Not Found</h1> -->
+      <p class='error__msg'>Sorry, the requested url could not be found.</p>
+      <router-link to="/">Go back to Home page</router-link>
+    </main>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ErrorPage'
+  };
+  </script>
+  
+  <style>
+  /* ... */
+  .error{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    height: calc(100vh - 80px);
+    width: 98%;
+    margin: 0 auto;
+  }
+  .image_wrapper{
+    width: 400px;
+    height: 300px;
+  }
+  .image_wrapper img{
+    width: 100%;
+  }
+  .heading-main{
+    font-size: 48px;
+    margin-bottom: 16px;
+    text-align: center;
+    font-family: primaryFont;
+    font-weight: bolder;
+  }
+  .error__msg{
+    font-family: terFont;
+    font-size: 16px;
+    text-align: center;
+    color: red;
+  }
+  </style>
